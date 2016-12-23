ce40fa2c3d1a045d6f0ad100c8dc8ff016584b3a
Philip Vo <philiptranbavo@gmail.com>
2016-12-22 19:11:03 -0800

    More refactoring.

--- dirtdb.sql
+++ dirtdb.sql
@@ -46,7 +46,6 @@ CREATE TABLE `applications` (
 
 LOCK TABLES `applications` WRITE;
 /*!40000 ALTER TABLE `applications` DISABLE KEYS */;
-INSERT INTO `applications` VALUES ('+Σ®\Δ\γζª H\Β|2',1,'2016-12-17 21:30:34','2016-12-17 21:30:34','C\θ€\έ\Δ\άζª H\Β|2','aΐ?\Δ\Οζª H\Β|2'),('H\ΡZ\Ζ0ζª H\Β|2',0,'2016-12-19 13:14:43','2016-12-19 13:14:43','3•:\Ζ0ζª H\Β|2','aΐ?\Δ\Οζª H\Β|2'),('LS\Ζ3ζª H\Β|2',0,'2016-12-19 13:36:17','2016-12-19 13:36:17','3•:\Ζ0ζª H\Β|2','ƒό“\Ζ0ζª H\Β|2'),('®υ\ή6\Ζ4ζª H\Β|2',0,'2016-12-19 13:46:12','2016-12-19 13:46:12','3•:\Ζ0ζª H\Β|2','ªV\άs\Ζ4ζª H\Β|2');
 /*!40000 ALTER TABLE `applications` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -77,7 +76,6 @@ CREATE TABLE `dropoff` (
 
 LOCK TABLES `dropoff` WRITE;
 /*!40000 ALTER TABLE `dropoff` DISABLE KEYS */;
-INSERT INTO `dropoff` VALUES ('1105 Cantara Court','San Jose','CA',95127,1,'3•:\Ζ0ζª H\Β|2'),('1920 Zanker Road','San Jose','CA',95616,1,'C\θ€\έ\Δ\άζª H\Β|2'),('1105 Cantara Court','San Jose','CA',95127,1,'·\μπ\Δ\Ϊζª H\Β|2');
 /*!40000 ALTER TABLE `dropoff` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -120,6 +118,7 @@ DROP TABLE IF EXISTS `jobs`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `jobs` (
   `id` binary(16) NOT NULL,
+  `job_status` tinyint(1) NOT NULL,
   `job_type` tinyint(1) NOT NULL,
   `dirt_type` varchar(45) NOT NULL,
   `volume` decimal(10,2) NOT NULL,
@@ -140,7 +139,6 @@ CREATE TABLE `jobs` (
 
 LOCK TABLES `jobs` WRITE;
 /*!40000 ALTER TABLE `jobs` DISABLE KEYS */;
-INSERT INTO `jobs` VALUES ('\"x\Ο\Ζ0ζª H\Β|2',0,'Recycled Concrete',234.00,'2016-12-24 08:00:00','2016-12-19 13:13:29','2016-12-19 13:13:29','”•j\Δ\Οζª H\Β|2'),('3•:\Ζ0ζª H\Β|2',1,'Topsoil - Average',234664.00,'2016-12-31 08:00:00','2016-12-19 13:14:07','2016-12-19 13:14:07','”•j\Δ\Οζª H\Β|2'),('C\θ€\έ\Δ\άζª H\Β|2',2,'Topsoil - Economy',3421.23,'2016-12-17 08:00:00','2016-12-17 20:41:08','2016-12-17 20:41:08','”•j\Δ\Οζª H\Β|2'),('·\μπ\Δ\Ϊζª H\Β|2',1,'Recycled Asphalt',123.00,'2016-12-31 08:00:00','2016-12-17 20:30:03','2016-12-17 20:30:03','”•j\Δ\Οζª H\Β|2'),('όs\r\Δ\ίζª H\Β|2',0,'Rough Fill',123.00,'2016-12-22 08:00:00','2016-12-17 21:07:47','2016-12-17 21:07:47','ρ\λ[\Δ\ίζª H\Β|2'),('ÿW±-\Δ\ζΉρ H\Β|2',0,'Clean Fill',0.01,'2016-12-31 08:00:00','2016-12-16 20:11:22','2016-12-16 20:11:22','\μ\ΤGo\Δ\ζΉρ H\Β|2');
 /*!40000 ALTER TABLE `jobs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -176,7 +174,6 @@ CREATE TABLE `messages` (
 
 LOCK TABLES `messages` WRITE;
 /*!40000 ALTER TABLE `messages` DISABLE KEYS */;
-INSERT INTO `messages` VALUES ('2¶\\Ζζª H\Β|2','asdasds','2016-12-19 22:52:58','2016-12-19 22:52:58','®υ\ή6\Ζ4ζª H\Β|2',NULL,'ªV\άs\Ζ4ζª H\Β|2'),('\ΫωvΖζª H\Β|2','asdsa','2016-12-19 22:53:03','2016-12-19 22:53:03','®υ\ή6\Ζ4ζª H\Β|2',NULL,'ªV\άs\Ζ4ζª H\Β|2'),('+\Ϋi0Ζƒζª H\Β|2','check 1111','2016-12-19 23:08:02','2016-12-19 23:08:02','®υ\ή6\Ζ4ζª H\Β|2','”•j\Δ\Οζª H\Β|2',NULL),('/Rw²\Ζ|ζª H\Β|2','1105 Cantara Court','2016-12-19 22:18:02','2016-12-19 22:18:02','H\ΡZ\Ζ0ζª H\Β|2','”•j\Δ\Οζª H\Β|2',NULL),('p!b\Π\Ζόζª H\Β|2','hi','2016-12-20 13:35:42','2016-12-20 13:35:42','®υ\ή6\Ζ4ζª H\Β|2','”•j\Δ\Οζª H\Β|2',NULL),('€\n£\αΖ‚ζª H\Β|2','asdsa','2016-12-19 23:03:14','2016-12-19 23:03:14','®υ\ή6\Ζ4ζª H\Β|2',NULL,'ªV\άs\Ζ4ζª H\Β|2'),('„qU<Ζ‚ζª H\Β|2','asdasdasd','2016-12-19 23:03:21','2016-12-19 23:03:21','®υ\ή6\Ζ4ζª H\Β|2',NULL,'ªV\άs\Ζ4ζª H\Β|2'),('Ϊ§Ζ‚ζª H\Β|2','asfcavasva','2016-12-19 23:03:29','2016-12-19 23:03:29','®υ\ή6\Ζ4ζª H\Β|2',NULL,'ªV\άs\Ζ4ζª H\Β|2'),('R\μ‘Ζ‚ζª H\Β|2','asdas','2016-12-19 23:03:31','2016-12-19 23:03:31','®υ\ή6\Ζ4ζª H\Β|2','”•j\Δ\Οζª H\Β|2',NULL),('’\nΖƒζª H\Β|2','dasdsa','2016-12-19 23:10:54','2016-12-19 23:10:54','®υ\ή6\Ζ4ζª H\Β|2','”•j\Δ\Οζª H\Β|2',NULL),('°S$Ζ‚ζª H\Β|2','asdasd','2016-12-19 23:04:05','2016-12-19 23:04:05','®υ\ή6\Ζ4ζª H\Β|2',NULL,'ªV\άs\Ζ4ζª H\Β|2'),('Ύ—S―\Ζζª H\Β|2','Hey Minh','2016-12-19 22:43:30','2016-12-19 22:43:30','®υ\ή6\Ζ4ζª H\Β|2','”•j\Δ\Οζª H\Β|2',NULL),('ΐ΄ª\Ζ|ζª H\Β|2','1105 Cantara Court San Jose, CA','2016-12-19 22:22:05','2016-12-19 22:22:05','H\ΡZ\Ζ0ζª H\Β|2','”•j\Δ\Οζª H\Β|2',NULL),('\Β\β\λ,\Ζζª H\Β|2','hi','2016-12-19 22:43:38','2016-12-19 22:43:38','®υ\ή6\Ζ4ζª H\Β|2',NULL,'ªV\άs\Ζ4ζª H\Β|2'),('\Ζ[΅)Ε±ζª H\Β|2','sdafsd','2016-12-18 22:09:30','2016-12-18 22:09:30','+Σ®\Δ\γζª H\Β|2','”•j\Δ\Οζª H\Β|2',NULL),('Θ¤B\ίΖ‚ζª H\Β|2','vsdfsad','2016-12-19 23:05:16','2016-12-19 23:05:16','®υ\ή6\Ζ4ζª H\Β|2',NULL,'ªV\άs\Ζ4ζª H\Β|2'),('Οƒ\Φ4Ζζª H\Β|2','asdsa','2016-12-19 22:58:18','2016-12-19 22:58:18','®υ\ή6\Ζ4ζª H\Β|2','”•j\Δ\Οζª H\Β|2',NULL),('\ί\Ωkw\Ζζª H\Β|2','hi Jess','2016-12-19 22:44:26','2016-12-19 22:44:26','®υ\ή6\Ζ4ζª H\Β|2',NULL,'ªV\άs\Ζ4ζª H\Β|2'),('\ηE²¤Ζ‰ζª H\Β|2','DOOOOOOOONNNNNNNE','2016-12-19 23:56:14','2016-12-19 23:56:14','®υ\ή6\Ζ4ζª H\Β|2','”•j\Δ\Οζª H\Β|2',NULL),('\μC!Ζ€ζª H\Β|2','hey','2016-12-19 22:51:57','2016-12-19 22:51:57','®υ\ή6\Ζ4ζª H\Β|2',NULL,'ªV\άs\Ζ4ζª H\Β|2'),('\ξΑ—–\Ζζª H\Β|2','Check 123','2016-12-19 22:44:51','2016-12-19 22:44:51','®υ\ή6\Ζ4ζª H\Β|2','”•j\Δ\Οζª H\Β|2',NULL),('φ\οY\Μ\Ζζª H\Β|2','This is a longer messageThis is a longer messageThis is a longer messageThis is a longer messageThis is a longer message asd','2016-12-19 22:45:05','2016-12-19 22:45:05','®υ\ή6\Ζ4ζª H\Β|2','”•j\Δ\Οζª H\Β|2',NULL);
 /*!40000 ALTER TABLE `messages` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -207,7 +204,6 @@ CREATE TABLE `pickup` (
 
 LOCK TABLES `pickup` WRITE;
 /*!40000 ALTER TABLE `pickup` DISABLE KEYS */;
-INSERT INTO `pickup` VALUES ('1105 Cantara Court','San Jose','CA',95127,1,'\"x\Ο\Ζ0ζª H\Β|2'),('1105 Cantara Court','San Jose','CA',95127,0,'C\θ€\έ\Δ\άζª H\Β|2'),('1105 Cantara Court','San Jose','CA',95127,1,'όs\r\Δ\ίζª H\Β|2'),('1105 Cantara Court','San Jose','CA',95127,1,'ÿW±-\Δ\ζΉρ H\Β|2');
 /*!40000 ALTER TABLE `pickup` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -243,7 +239,7 @@ CREATE TABLE `truckers` (
 
 LOCK TABLES `truckers` WRITE;
 /*!40000 ALTER TABLE `truckers` DISABLE KEYS */;
-INSERT INTO `truckers` VALUES ('aΐ?\Δ\Οζª H\Β|2','sonny@tosco.com','Sonny','Tosco','$2a$10$b6/F.tutBf434cnXXSfLZ.KHdIM335bYFzylpwWDhhhXGiDavNNJm',0,'Honda','Truck',1990,'asdfg','2016-12-17 19:08:55','2016-12-17 19:08:55'),('ƒό“\Ζ0ζª H\Β|2','alex@wap.com','Alex','Wap','$2a$10$cbqG6wB7gSPDGdpj9jrZbeutbT3wggVhgolPvfPL6b3Jw.0rxSnQa',1,'Nissan','Honda',1980,NULL,'2016-12-19 13:16:22','2016-12-19 13:16:22'),('ªV\άs\Ζ4ζª H\Β|2','minh@pham.com','Minh','Pham','$2a$10$ATZCa87UaO8BexzQ1edUPe6XazvgZtc1IRemgo5I95AXPsu9XzYC.',0,'Something','Something',1980,NULL,'2016-12-19 13:46:04','2016-12-19 13:46:04');
+INSERT INTO `truckers` VALUES ('aΐ?\Δ\Οζª H\Β|2','sonny@tosco.com','Sonny','Tosco','$2a$10$b6/F.tutBf434cnXXSfLZ.KHdIM335bYFzylpwWDhhhXGiDavNNJm',0,'Honda','Truck',1990,'asdfg','2016-12-17 19:08:55','2016-12-17 19:08:55'),('ƒό“\Ζ0ζª H\Β|2','alex@wap.com','Alex','Wap','$2a$10$cbqG6wB7gSPDGdpj9jrZbeutbT3wggVhgolPvfPL6b3Jw.0rxSnQa',1,'Nissan','Honda',1980,NULL,'2016-12-19 13:16:22','2016-12-19 13:16:22'),('ªV\άs\Ζ4ζª H\Β|2','minh@pham.com','Minh','Pham','$2a$10$ATZCa87UaO8BexzQ1edUPe6XazvgZtc1IRemgo5I95AXPsu9XzYC.',0,'Something','Something',1980,NULL,'2016-12-19 13:46:04','2016-12-19 13:46:04'),('³¶^\μΘ\ζ² H\Β|2','mkj@non.com','mjk','mk','$2a$10$.4F/fUlfdPBThGG9x1qhR.i9RiLVDACEGJZnYqVZ1R9mF2OsO8itW',0,'vf','bd',2002,NULL,'2016-12-22 13:36:30','2016-12-22 13:36:30'),('ΐ8ΑZ\Θ\ζ² H\Β|2','vr@vsd.co','vd','ge','$2a$10$XKmspl9M/rD47QbF8gqPjuKfjYyNQ96jdmOrvOsuqlzmKYV3SlAgi',0,'vsd','mko',1980,NULL,'2016-12-21 23:39:20','2016-12-21 23:39:20');
 /*!40000 ALTER TABLE `truckers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -274,7 +270,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('”•j\Δ\Οζª H\Β|2','jessica@ganda.com','Jessica','Ganda','$2a$10$YRDOiOlX/Aok.dWmwT8iNeOafr1sykDLkeS7ep4ExgTLdc/lAMq2m','2016-12-17 19:10:07','2016-12-17 19:10:07'),('\μ\ΤGo\Δ\ζΉρ H\Β|2','philiptranbavo@gmail.com','Philip','Vo','$2a$10$T6uSB/x5Q1IDijJCCKvRhup7wWimHaQblNNqdyWJgVP56L57HBNau','2016-12-16 20:10:51','2016-12-16 20:10:51'),('ρ\λ[\Δ\ίζª H\Β|2','elliot@young.com','Elliot','Young','$2a$10$GtYWM1sI6.bmRj4Q1OPqI.78qO.xHxq2xVCWBVYWONh4kEC0Q6ppS','2016-12-17 21:07:29','2016-12-17 21:07:29');
+INSERT INTO `users` VALUES ('\\U\Θ\Z\ζ² H\Β|2','v@sdv.cs','ber','ber','$2a$10$FbUnohbKAY8PHVnYIMZO4uRS8ZpvD9ZZxH1CBdjoFMNHGTMTXT7ze','2016-12-21 23:41:18','2016-12-21 23:41:18'),('\ε\"½\Θ\ζ² H\Β|2','ph@vo.com','Ph','Vo','$2a$10$gwhhcopBT0BL9ynCirM6ieWeSUByzTANbzTmY1uaEurItL/YwgyZe','2016-12-21 23:34:48','2016-12-21 23:34:48'),(' \Χ\ξ‰\Θ\ζ² H\Β|2','ca@la.com','vas','al','$2a$10$zF0X5lTl8MRypo1FYY5HBuh.HHTJ/miuXsy7NlHsv76BcxBlMbZay','2016-12-22 00:10:40','2016-12-22 00:10:40'),('.rΆ\Θ\ζ² H\Β|2','jinny@kim.com','Jinny','Kim','$2a$10$K9l5YgreuP0C1J74RCb.Ouj3fvU.1s7g53vjpxzQ3oondRL5YcQdO','2016-12-21 22:38:00','2016-12-21 22:38:00'),('/\α?\Θ\ζ² H\Β|2','je@ga.com','Je','Ga','$2a$10$bpnUPUnlTnryT1uUMV0ELedpPd0H/1rpGdkRasrEWTxbRR1QzZWpi','2016-12-21 23:35:18','2016-12-21 23:35:18'),('~iϋe\Θ\ζ² H\Β|2','philip@vo.vom','Philip','Vo','$2a$10$sDthJwf2NW3f1A0wYm5iHeCRc94L.VHnyk5x.BFUIjq4jNDCmg8KK','2016-12-21 23:30:20','2016-12-21 23:30:20'),('U\θ’\Θ\ζ² H\Β|2','moi@moi.c','mi','moi','$2a$10$N3XMdnQQvAau.NliXl/i/O6oDR/02tbepJ19ZPKt7Pr87o1V63mT2','2016-12-22 00:13:19','2016-12-22 00:13:19'),('”•j\Δ\Οζª H\Β|2','jessica@ganda.com','Jessica','Ganda','$2a$10$YRDOiOlX/Aok.dWmwT8iNeOafr1sykDLkeS7ep4ExgTLdc/lAMq2m','2016-12-17 19:10:07','2016-12-17 19:10:07'),('«—y\\\Θ\ζ² H\Β|2','apsa@as.co','asd','acca','$2a$10$SwLAvr5HmvvxL1nfXiqOreIsiQTH9A7jrEl5A.kIEwam7uXMepy6G','2016-12-21 23:38:45','2016-12-21 23:38:45'),('½+tΘ\ζ² H\Β|2','be@vsd.vom','vd','ber','$2a$10$Ft4busmCRK5RsJVL4ESrSeLTGCDO3UxwPbkXIfKp1dAmkQn39vTCG','2016-12-22 13:36:47','2016-12-22 13:36:47'),('\Ιpό\Θ\ζ² H\Β|2','vsd@vsd.sd','vs','vsd','$2a$10$6W/Gg8xBybW4LDaZBhFBj.z1aIVSkGsz7kftfj2ekJ4wbVGVQ3RtG','2016-12-22 00:08:14','2016-12-22 00:08:14'),('\Ο\Χ2¶\Θ\ζ² H\Β|2','vsd@vssad.sd','vs','vsd','$2a$10$S8tGCDDrH8vI1eiGS.RrIeLQrkh.WvRGKreEOQqrFCoyWSy/XjIgi','2016-12-22 00:08:24','2016-12-22 00:08:24'),('Τ”\Ι\Θ\ζ² H\Β|2','vsd@o.xio','bds','bsaf','$2a$10$WYdOqN/6ijL5jhZHSLXorOuMJ1WCmMwdPVPjtWJVuhyrbyqDU3/te','2016-12-21 23:39:54','2016-12-21 23:39:54'),('Φ€ψ\Θ\ζ² H\Β|2','al@wa.com','Al','Wa','$2a$10$ZNTx0sUurcw4JYjDrdxb6OEY36y3.YcShN9.dL61z9KuYvvM7iR5y','2016-12-21 23:32:48','2016-12-21 23:32:48'),('\μ\ΤGo\Δ\ζΉρ H\Β|2','philiptranbavo@gmail.com','Philip','Vo','$2a$10$T6uSB/x5Q1IDijJCCKvRhup7wWimHaQblNNqdyWJgVP56L57HBNau','2016-12-16 20:10:51','2016-12-16 20:10:51'),('ρ\λ[\Δ\ίζª H\Β|2','elliot@young.com','Elliot','Young','$2a$10$GtYWM1sI6.bmRj4Q1OPqI.78qO.xHxq2xVCWBVYWONh4kEC0Q6ppS','2016-12-17 21:07:29','2016-12-17 21:07:29');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -287,4 +283,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-12-20 15:02:00
+-- Dump completed on 2016-12-22 19:10:55
