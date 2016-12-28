bc4a787188365e37796c0363c10a1ec28d39e13e
George Miranda <george92miranda@gmail.com>
2016-12-28 12:04:07 -0800

    save for relocation

--- client/assets/controllers/settingsController.js
+++ client/assets/controllers/settingsController.js
@@ -10,9 +10,9 @@ app.controller('settingsController',function ($scope, $location, $routeParams, u
       factory = truckersFactory;
 
     factory.show($scope.id, function(data){
-      // $scope.first_name = data.first_name;
-      // $scope.last_name = data.last_name;
+
       console.log(data);
+			$scope.tab = 'profile';
     });
 	}
 	else
